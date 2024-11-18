a34d29f9fedbeee41908d19372e4387527cb44a5
Aaron Archaga <Aaron Archaga@Laptop_Archaga>
2024-11-18 10:57:01 -0600

    EJERCICIO 1 DE EMPLEADOS

---
+++ src/claseprogramacion/semana6/ejercicios/jueves/PrincipalEmpleado.java
@@ -0,0 +1,29 @@
+package claseprogramacion.semana6.ejercicios.jueves;
+
+import java.util.Scanner;
+
+public class PrincipalEmpleado {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        EmpleadoBonificacion empleado = new EmpleadoBonificacion("",0,"",0,0);
+        
+        System.out.println("Ingrese el primer nombre: ");
+        empleado.setNombre(sc.next());
+        
+        System.out.println("Ingrese el salario: ");
+        empleado.setSalario(sc.nextDouble());
+        
+        System.out.println("Ingrese el genero: ");
+        empleado.setGenero(sc.next());
+        
+        System.out.println("Ingrese la edad: ");
+        empleado.setEdad(sc.nextInt());
+        
+        System.out.println("Ingrese la antiguedad: ");
+        empleado.setAntiguedad(sc.nextInt());
+        
+        //Mostras la info
+        System.out.println("La info es:");
+        empleado.mostrarinfo();
+    }
+}
